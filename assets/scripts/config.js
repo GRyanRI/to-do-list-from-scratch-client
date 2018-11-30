be008ea5b422f8b8d351ef205118f32c63d1befb
GRyanRI <gregorywryan@gmail.com>
2018-11-30 08:30:24 -0500

    final commit before deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://gentle-meadow-68711.herokuapp.com/',
+  production: 'https://gentle-meadow-68711.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
